0ef4d4e4065dd4cd0ec971c45d2e0260a15c7a0f
Ayanacyan <amitpacl@gmail.com>
2025-12-25 21:18:46 -0600

    type casting

---
+++ typecasting.py
@@ -0,0 +1,26 @@
+name="Ayana"
+age=12
+is_student=True
+height= 5.2
+
+print("Name:", name)
+print("Datatype of name is:", type(name))
+
+print("Age:", age)
+print("Datatype of age is:", type(age))
+
+print("Is a student?:", is_student)
+print("Datatype of is_student is:", type(is_student))
+
+print("height:", height)
+print("Datatype of height is:", type(height))
+
+print("\n After type casting:")
+age=str(age)
+print(age)
+print("Datatype of age is:", type(age))
+
+height=int(height)
+print(height)
+print("Datatype of height is:", type(height))
+

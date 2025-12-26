4428b0e4081e60ed69f1785991f7d0dbc5ab9996
Ayanacyan <amitpacl@gmail.com>
2025-12-26 17:09:45 -0600

    More string operations

---
+++ string operation hw.py
@@ -0,0 +1,13 @@
+message=str("congratulations")
+print(message.upper())
+
+print(message.lower())
+
+revText=message[::-1]
+text=revText
+print(text)
+
+str1="Cong"
+str2="ratulations"
+print(str1+str2)
+
